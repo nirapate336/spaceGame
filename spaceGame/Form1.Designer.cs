e0f9219026cb1158bbbd7f30611e96e3fb39fd58
Nirav Patel <nirapate336@outlook.com>
2021-03-23 09:25:27 -0400

    Add project files.

---
+++ spaceGame/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace spaceGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.playerOneScoreLabel = new System.Windows.Forms.Label();
+            this.playerTwoScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.titleLabel.Location = new System.Drawing.Point(165, 98);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(0, 22);
+            this.titleLabel.TabIndex = 0;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.AutoSize = true;
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.subTitleLabel.Location = new System.Drawing.Point(88, 153);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(0, 22);
+            this.subTitleLabel.TabIndex = 1;
+            // 
+            // playerOneScoreLabel
+            // 
+            this.playerOneScoreLabel.AutoSize = true;
+            this.playerOneScoreLabel.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerOneScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.playerOneScoreLabel.Location = new System.Drawing.Point(40, 283);
+            this.playerOneScoreLabel.Name = "playerOneScoreLabel";
+            this.playerOneScoreLabel.Size = new System.Drawing.Size(30, 32);
+            this.playerOneScoreLabel.TabIndex = 2;
+            this.playerOneScoreLabel.Text = "0";
+            // 
+            // playerTwoScoreLabel
+            // 
+            this.playerTwoScoreLabel.AutoSize = true;
+            this.playerTwoScoreLabel.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playerTwoScoreLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.playerTwoScoreLabel.Location = new System.Drawing.Point(517, 283);
+            this.playerTwoScoreLabel.Name = "playerTwoScoreLabel";
+            this.playerTwoScoreLabel.Size = new System.Drawing.Size(30, 32);
+            this.playerTwoScoreLabel.TabIndex = 3;
+            this.playerTwoScoreLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.playerTwoScoreLabel);
+            this.Controls.Add(this.playerOneScoreLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label playerOneScoreLabel;
+        private System.Windows.Forms.Label playerTwoScoreLabel;
+    }
+}
+
---
+++ spaceGame/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spaceGame
+{
+    public partial class Form1 : Form
+    {
+
+        List<int> obstacleYLeftList = new List<int>();
+        List<int> obstacleLeftSpeed = new List<int>();
+        List<int> obstacleXLeftList = new List<int>();
+
+        List<int> obstacleYRightList = new List<int>();
+        List<int> obstacleRightSpeed = new List<int>();
+        List<int> obstacleXRightList = new List<int>();
+
+
+
+
+
+
+
+
+
+
+        int obstacleHeightList = 2;
+        int obstacleWidthList = 10;
+        Random randGen = new Random();
+        int randValue;
+
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upDown = false;
+        bool downDown = false;
+
+        int player1X = 200;
+        int player1Y = 320;
+        int heroHeight = 20;
+        int heroWidth = 5;
+        int heroSpeed = 5;
+        int player2X = 300;
+        int player2Y = 320;
+
+        int oneScore;
+        int twoScore;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        Pen whitePen = new Pen(Color.White);
+
+        string gameState = "waiting";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upDown = true;
+                    break;
+
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upDown = false;
+                    break;
+
+                case Keys.Down:
+                    downDown = false;
+                    break;
+
+                case Keys.W:
+                    wDown = false;
+                    break;
+
+                case Keys.S:
+                    sDown = false;
+                    break;
+            }
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(whiteBrush, player1X, player1Y, heroWidth, heroHeight);
+            e.Graphics.FillRectangle(whiteBrush, player2X, player2Y, heroWidth, heroHeight);
+
+            for (int i = 0; i < obstacleYLeftList.Count(); i++)
+            {
+
+
+                e.Graphics.FillRectangle(whiteBrush, obstacleXLeftList[i], obstacleYLeftList[i], obstacleWidthList, obstacleHeightList);
+
+            }
+            for (int n = 0; n < obstacleYRightList.Count; n++)
+            {
+                e.Graphics.FillRectangle(whiteBrush, obstacleXRightList[n], obstacleYRightList[n], obstacleWidthList, obstacleHeightList);
+            }
+        }
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            // move player 1 and 2 up and down and keep them on screen
+            if (upDown == true && player2Y > 0)
+            {
+                player2Y -= heroSpeed;
+            }
+
+            if (downDown == true && player2Y < 330)
+            {
+                player2Y += heroSpeed;
+            }
+
+            if (wDown == true && player1Y > 0)
+            {
+                player1Y -= heroSpeed;
+            }
+
+            if (sDown == true && player1Y < 330)
+            {
+                player1Y += heroSpeed;
+            }
+
+            // check to see if we need to add asteroid
+            randValue = randGen.Next(0, 101);
+            if (randValue <= 5)
+            {
+
+                obstacleYLeftList.Add(randGen.Next(2, 300));
+                obstacleLeftSpeed.Add(randGen.Next(2, 5));
+                obstacleXLeftList.Add(0);
+
+            }
+            else if (randValue <= 11)
+            {
+                obstacleYRightList.Add(randGen.Next(2, 300));
+                obstacleRightSpeed.Add(randGen.Next(2, 5));
+                obstacleXRightList.Add(500);
+            }
+
+            //move asteriod by its given speed
+
+            for (int i = 0; i < obstacleYLeftList.Count(); i++)
+            {
+                obstacleXLeftList[i] += obstacleLeftSpeed[i];
+            }
+            for (int i = 0; i < obstacleYRightList.Count(); i++)
+            {
+                obstacleXRightList[i] -= obstacleRightSpeed[i];
+            }
+            // give points if they reach the top of the game
+            if (player1Y < 20)
+            {
+                oneScore++;
+                playerOneScoreLabel.Text = $"{oneScore}";
+                player1Y = 320;
+            }
+            if (player2Y < 20)
+            {
+                twoScore++;
+                playerTwoScoreLabel.Text = $"{twoScore}";
+                player2Y = 320;
+            }
+
+
+            //check for collisions between the asteriods and the play
+            Rectangle player1Rec = new Rectangle(player1X, player1Y, heroWidth, heroHeight);
+            Rectangle player2Rec = new Rectangle(player2X, player2Y, heroWidth, heroHeight);
+
+            for (int i = 0; i < obstacleYLeftList.Count(); i++)
+            {
+                Rectangle obstacleLeftRec = new Rectangle(obstacleXLeftList[i], obstacleYLeftList[i], obstacleWidthList, obstacleHeightList);
+               // Rectangle obstacleRightRec = new Rectangle(obstacleXRightList[i], obstacleYRightList[i], obstacleWidthList, obstacleHeightList);
+
+                if (player1Rec.IntersectsWith(obstacleLeftRec))//|| player1Rec.IntersectsWith(obstacleRightRec))
+                {
+                    player1Y = 320;
+                }
+               else if (player2Rec.IntersectsWith(obstacleLeftRec)) //|| player2Rec.IntersectsWith(obstacleRightRec))
+                {
+                    player2Y = 320;
+                }
+               
+                break;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (oneScore == 3 || twoScore == 3)
+            {
+                gameTimer.Enabled = false;
+
+            }
+
+
+
+            Refresh();
+
+        }
+
+    }
+}
